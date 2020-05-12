c20abc8a2217cebcbe306b47d4637bf8b64d0fd0
David M Williams <david@davidmwilliams.com>
2020-05-12 14:44:42 +1000

    Simple UI to start

---
+++ SecurityExplorer/MainForm.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace SecurityExplorer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuBar = new System.Windows.Forms.MenuStrip();
+            this.FileMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Splitter = new System.Windows.Forms.SplitContainer();
+            this.FileTree = new System.Windows.Forms.TreeView();
+            this.StatusBar = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutSecurityExplorerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuBar.SuspendLayout();
+            this.Splitter.Panel1.SuspendLayout();
+            this.Splitter.SuspendLayout();
+            this.StatusBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuBar
+            // 
+            this.MenuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileMenu,
+            this.helpToolStripMenuItem});
+            this.MenuBar.Location = new System.Drawing.Point(0, 0);
+            this.MenuBar.Name = "MenuBar";
+            this.MenuBar.Size = new System.Drawing.Size(1028, 24);
+            this.MenuBar.TabIndex = 1;
+            // 
+            // FileMenu
+            // 
+            this.FileMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitToolStripMenuItem});
+            this.FileMenu.Name = "FileMenu";
+            this.FileMenu.Size = new System.Drawing.Size(37, 20);
+            this.FileMenu.Text = "File";
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ExitToolStripMenuItem.Text = "Exit";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // Splitter
+            // 
+            this.Splitter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Splitter.Location = new System.Drawing.Point(0, 24);
+            this.Splitter.Name = "Splitter";
+            // 
+            // Splitter.Panel1
+            // 
+            this.Splitter.Panel1.Controls.Add(this.FileTree);
+            this.Splitter.Size = new System.Drawing.Size(1028, 691);
+            this.Splitter.SplitterDistance = 279;
+            this.Splitter.TabIndex = 2;
+            // 
+            // FileTree
+            // 
+            this.FileTree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FileTree.Location = new System.Drawing.Point(0, 0);
+            this.FileTree.Name = "FileTree";
+            this.FileTree.Size = new System.Drawing.Size(279, 691);
+            this.FileTree.TabIndex = 0;
+            // 
+            // StatusBar
+            // 
+            this.StatusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            this.StatusBar.Location = new System.Drawing.Point(0, 693);
+            this.StatusBar.Name = "StatusBar";
+            this.StatusBar.Size = new System.Drawing.Size(1028, 22);
+            this.StatusBar.TabIndex = 3;
+            this.StatusBar.Text = "statusStrip1";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(42, 17);
+            this.StatusLabel.Text = "Ready.";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutSecurityExplorerToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutSecurityExplorerToolStripMenuItem
+            // 
+            this.aboutSecurityExplorerToolStripMenuItem.Name = "aboutSecurityExplorerToolStripMenuItem";
+            this.aboutSecurityExplorerToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.aboutSecurityExplorerToolStripMenuItem.Text = "About Security Explorer";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 715);
+            this.Controls.Add(this.StatusBar);
+            this.Controls.Add(this.Splitter);
+            this.Controls.Add(this.MenuBar);
+            this.MainMenuStrip = this.MenuBar;
+            this.Name = "MainForm";
+            this.Text = "Security Explorer";
+            this.MenuBar.ResumeLayout(false);
+            this.MenuBar.PerformLayout();
+            this.Splitter.Panel1.ResumeLayout(false);
+            this.Splitter.ResumeLayout(false);
+            this.StatusBar.ResumeLayout(false);
+            this.StatusBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuBar;
+        private System.Windows.Forms.ToolStripMenuItem FileMenu;
+        private System.Windows.Forms.SplitContainer Splitter;
+        private System.Windows.Forms.TreeView FileTree;
+        private System.Windows.Forms.StatusStrip StatusBar;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutSecurityExplorerToolStripMenuItem;
+    }
+}
+
---
+++ SecurityExplorer/MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SecurityExplorer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
